071e65983bcf9d5d448aa07acbd2b732a961e7fa
Michael Zapf <michael.zapf@fau.de>
2016-04-09 21:09:15 +0200

    host: improved setup script

--- usb-host/setup-tap.sh
+++ usb-host/setup-tap.sh
@@ -2,18 +2,19 @@
 
 ARGC=$#
 
-if [ $ARGC -lt 3 ]; then
-    echo "Usage: $0 user group device" 1>&2
+if [ $ARGC -lt 4 ]; then
+    echo "Usage: $0 user group device ip" 1>&2
     exit
 fi
 
 USER=$1
 GROUP=$2
 NAME=$3
+IP=$4
 tunctl -u $USER -g $GROUP -t $NAME
 
 ip link set $NAME up
-ip addr add 10.254.0.1/24 dev $NAME
+ip addr add $IP/24 dev $NAME
 ip link set dev $NAME arp off
 
 sysctl -w net.ipv4.conf.${NAME}.rp_filter=2
