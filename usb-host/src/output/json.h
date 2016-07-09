0c113375ab7461b0056e1eda6c79fe79222b19f8
Michael Zapf <michael.zapf@fau.de>
2016-07-09 19:16:41 +0200

    added compatibility for windows host

--- usb-host/src/output/json.h
+++ usb-host/src/output/json.h
@@ -14,6 +14,10 @@
 #include <fstream>
 #include "output.h"
 
+#ifndef M_PI
+#define M_PI           3.14159265358979323846
+#endif
+
 class JsonOutput : public DataOutput {
     public:
         JsonOutput(std::string filename);
